5d900ad8adc3ed93a5b68035402ccdf6b975183d
anthonyss09 <76715818+anthonyss09@users.noreply.github.com>
2024-01-15 15:47:12 -0500

    nodejs server listening

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+//dotenv is a module that loads environment variables from a .env file into process.env
+import * as dotenv from "dotenv";
+
+const port = process.env.PORT || 8080;
+
+//express.json() is a built-in middleware function in express. It parses incoming requests with JSON payloads and is based on body-parser.
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Our boiler plate nodejs server is sending!");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port:${port}`);
+});
